9f00ae04ac129e8bee3f15890cacdf74240a1798
mestock <MelissaStock44@gmail.com>
2019-03-05 15:19:28 -0800

    Completed Question 2

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 This program will ask a visitor 5 Yes/No questions about Melissa
 
 Q1:Is Melissa from the Greater Seattle area?
+Q2: Does Melissa enjoy dim sum for breakfast on sundays?
 
 */
 
@@ -13,12 +14,11 @@ Q1:Is Melissa from the Greater Seattle area?
 var possibleAnswers = ['yes', 'y', 'no', 'n'];
 
 //QUESTION 1: Is Melissa from Seattle?
+//Correct answer
+console.log('From Seattle Answer: ' + possibleAnswers[2]);
+
 //Create a variable for visitor response
 var fromSeattleGuess = prompt('Is Melissa from the Greater Seattle area?');
-
-//Create & store correct answer
-var fromSeattleAnswer = 'No';
-console.log('From Seattle Answer: ' + fromSeattleAnswer);
 
 //Store & edit response to lowercase
 console.log('From Seattle Guess: ' + fromSeattleGuess);
@@ -31,8 +31,21 @@ if(checkFromSeattleGuess === possibleAnswers[2] || checkFromSeattleGuess === pos
     alert('Nice guess, she\'s actually from Hawaii but stuck around after college.');
 };
 
-//QUESTION 2: Does enjoy dim sum for breakfast on sundays?
+//QUESTION 2: Does Melissa enjoy dim sum for breakfast on sundays?
+//Correct answer
+console.log('Eats Dim Sum Answer: ' + possibleAnswers[0]);
+
 //Create a variable for visitor response
+var dimSumGuess = prompt('Does Melissa enjoy dim sum for breakfast on sundays?');
+console.log('Eats dim sum guess: ' + dimSumGuess);
+var checkDimSumGuess = dimSumGuess.toLowerCase();
+
+//Compare response with correct answer
+if(checkDimSumGuess === possibleAnswers[0] || checkDimSumGuess === possibleAnswers[1]){
+    alert('YUM! Who doesn\'t love dim sum?!');
+}else{
+    alert('That would be insane');
+};
 
 //Q3:Is Melissa's favorite dog the Old Enlish Sheepdog?
 //Q4:Is snowboarding Melissa's favorite winter sport?
